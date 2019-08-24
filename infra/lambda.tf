c74b1c4cdbe8e4c2925f9ecfaa90ae61ebe56b58
Manuel <jmpcba@gmail.com>
2019-08-24 18:48:01 -0300

    elimino whitespaces

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -28,18 +28,18 @@ resource "aws_iam_role" "MG_presupuesto_lambda_role" {
         }
 
     assume_role_policy = <<POLICY
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": ""
-            }
-        ]
-        }
-        POLICY
-    }
+{
+"Version":"2012-10-17",
+"Statement":[
+{
+"Action": "sts:AssumeRole",
+"Principal":{
+"Service":"lambda.amazonaws.com"
+},
+"Effect":"Allow",
+"Sid":""
+}
+]
+}
+POLICY
+}
