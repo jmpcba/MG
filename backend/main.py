3dfbdcf73a9b9d63ca23a462802a45e2ae3752ce
Manuel <jmpcba@gmail.com>
2019-08-26 22:44:53 -0300

    typo en el response

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ def handler(event, context):
             'headers': {
                 'Content-Type': 'application/json', 
                 'Access-Control-Allow-Origin': '*' 
-                }
+                },
             'statusCode': 200,
             'body': "mail enviado"
             }
